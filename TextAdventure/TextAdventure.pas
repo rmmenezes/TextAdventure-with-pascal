f1b4b0befda3eb51a263a6f6e302d6220a1eecf9
rmmenezes <ra29fa@gmail.com>
2018-04-18 17:36:15 -0300

    apenas um começo

--- TextAdventure/TextAdventure.pas
+++ TextAdventure/TextAdventure.pas
@@ -1,10 +1,8 @@
-﻿
-{$mode objfpc}     // directive to be used for defining classes
+﻿{$mode objfpc}     // directive to be used for defining classes
 {$m+}		   // directive to be used for using constructor
 
 Program TextAdventure(output);
-uses CRT, sysutils;
-
+uses Process, CRT;
 //STRUCT OBJETO
 type Objeto = record
         id, tipo, cena_alvo: integer;
@@ -16,7 +14,8 @@ end;
 //STRUCT CENA
 type Cena = record
         id, qtd_objetos: integer;
-        titulo, descricao: string;
+        titulo : string;
+        descricao:ansistring;
         itens: array[0..10] of Objeto;
 end;
 
@@ -52,7 +51,7 @@ begin
     criaObjeto := obj;
 end;
 
-function criaCena(ident : integer; tit, desc : string; qtd_objetos : integer) : Cena;
+function criaCena(ident : integer; tit : string;  desc : ansistring; qtd_objetos : integer) : Cena;
 var
         scene : Cena;
 begin
@@ -67,6 +66,7 @@ function estaNoInv (inv : Inventario; obj : string) : boolean;
 var
         i : integer;
 begin
+        estaNoInv := false;
         for i := 0 to inv.freePos-1 do
         begin
                 if inv.itens[i].nome = obj then
@@ -74,10 +74,9 @@ begin
                         estaNoInv := true;
                 end;
         end;
-        estaNoInv := false;
 end;
 
-procedure removeDoInv (inv: Inventario, obj : string);
+function removeDoInv (inv: Inventario; obj : string) : Inventario;
 var
 	i : integer;
 begin
@@ -88,6 +87,7 @@ begin
 			inv.itens[i].obtido := false;
 		end;
 	end;
+        removeDoInv := inv;
 end;
 
 
@@ -107,13 +107,15 @@ end;
 function criaJogo () : Jogo;
 var
     arq: text;
-    linha, titulo, descricao_cena, descricao_objeto, nome, comando_correto, resultado_positivo, resultado_negativo : string;
+    linha, titulo, descricao_objeto, nome, comando_correto, resultado_positivo, resultado_negativo : string;
+    descricao_cena : ansistring;
     i, j, id_objeto, qtd_objetos, tipo, cena_alvo : integer;
     id_cena : integer;
     game : Jogo;
 
 
 begin
+        game.cena_atual := 0;
         Assign(arq,'./Files/Cenas.txt');
         reset(arq);
         i := 0;
@@ -130,8 +132,6 @@ begin
         //writeln(qtd_objetos);
 
         game.cenas[i] := criaCena(id_cena, titulo, descricao_cena, qtd_objetos);
-
-
    for j:=0 to qtd_objetos - 1 do
     begin
         readln(arq, id_objeto);
@@ -158,19 +158,13 @@ begin
 end;
 
 procedure printaCena (scene : Cena);
-var
-    i : integer;
+//var
+    //i : integer;
 begin
-        writeln('##XxCENAxX##');
         writeln(scene.titulo);
+        writeln();
         writeln(scene.descricao);
-        //writeln('##XxCENA-OBJETOSxX##');
-        //for i:=0 to scene.qtd_objetos-1 do
-        //begin
-            //writeln(scene.itens[i].nome);
-            //write('Comando Correto: ');
-            //writeln(scene.itens[i].comand_correct);
-        //end;
+        writeln();
 end;
 
 type
@@ -205,6 +199,23 @@ begin
   until Aux = '';
 end;
 
+procedure gameOver();
+var
+    arq : text;
+    linha : string;
+begin
+    reset(arq);
+    Assign(arq,'./Files/GameOver.txt');
+    while not EOF(arq) do
+        begin
+          readln(arq, linha);
+          writeln(linha);
+          Delay(150);
+    end;
+    close(arq);
+
+end;
+
 procedure PrintHelp();
 
 begin
@@ -213,28 +224,33 @@ begin
     writeln('use OBJETO -> interagir com objeto da cena (abrir, usar, pressionar, ...)');
     writeln('use ITEM with OBJETO -> usar item do inventário em objeto da cena');
     writeln('get OBJETO -> obtém objeto para o inventário');
+    writeln('save -> Salva o progresso do jogo');
     writeln('exit -> sai do jogo');
 end;
 
-procedure motorzera (game : Jogo);
+procedure motorzera (game : Jogo; cenaInicial : integer);
 var
-        kbImput : string; i, pos, controlPrintCena : integer;
+        kbImput : string;
+        i, controlPrintCena : integer;
         invent : Inventario;
+        arq : text;
 begin
         //show initial screen
-        game.cena_atual := 0;
+        kbImput := 'nada';
+        game.cena_atual := cenaInicial;
         controlPrintCena := 0;
         invent.freePos := 0;
 
         printaCena(game.cenas[game.cena_atual]);
-        writeln('escreva [help] para instrucões');
+        writeln('escreva [help] para instrucoes');
         while kbImput <> 'exit' do
         begin
             if game.cena_atual <> controlPrintCena then
             begin
+                    clrscr();
                     printaCena(game.cenas[game.cena_atual]);
                     controlPrintCena := game.cena_atual;
-                    writeln('escreva [help] para instrucões');
+                    writeln('escreva [help] para instrucoes');
             end;
             readln(kbImput);
 
@@ -242,113 +258,184 @@ begin
             begin
                     PrintHelp();
             end;
+            if kbImput = 'save' then
+            begin
+                   Assign(arq,'./Files/Save.txt');
+                   rewrite(arq); 
+                   writeln(arq, 'salve');
+                   writeln('Voce salvou o Jogo');
+            end;
+
             if kbImput = 'inventory' then
             begin
                     printaInventario(invent, invent.freePos);
             end;
 
-
             for i:=0 to game.cenas[game.cena_atual].qtd_objetos-1 do
             begin
-            	if (Split(kbImput, ' ').[0] = 'check') and (Split(kbImput, ' ').[0] = game.cenas[game.cena_atual].itens[i].nome) then
+            	if (Split(kbImput, ' ').[0] = 'check') and (Split(kbImput, ' ').[1] = game.cenas[game.cena_atual].itens[i].nome) then
                 begin
                 	writeln(game.cenas[game.cena_atual].itens[i].descricao);
                 end
-		else if (game.cenas[game.cena_atual].itens[i].tipo = 0) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
+		        else if (game.cenas[game.cena_atual].itens[i].tipo = 0) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
                 begin
                 	if game.cenas[game.cena_atual].itens[i].obtido = false then
-                        begin
-                        	game.cenas[game.cena_atual].itens[i].obtido := true;
-                                invent.itens[invent.freePos] := game.cenas[game.cena_atual].itens[i];
-                                invent.freePos := invent.FreePos + 1;
-                                writeln(game.cenas[game.cena_atual].itens[i].result_posit);
-                   	end
-			else
-			begin
-				writeln(game.cenas[game.cena_atual].itens[i].result_negat);
-			end;
+                    begin
+                        game.cenas[game.cena_atual].itens[i].obtido := true;
+                        invent.itens[invent.freePos] := game.cenas[game.cena_atual].itens[i];
+                        invent.freePos := invent.FreePos + 1;
+                        writeln(game.cenas[game.cena_atual].itens[i].result_posit);
+                   	end;
             	end
                 else if (game.cenas[game.cena_atual].itens[i].tipo = 1) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
-		begin
-                	if game.cenas[game.cena_atual].itens[i].resolvido = false then
-                        begin
-				game.cenas[game.cena_atual].itens[i].resolvido := true;
-				writeln(game.cenas[game.cena_atual].itens[i].result_posit);
-				game.cena_atual := game.cena_atual + 1;
-                        end
-			else
-			begin
-				writeln(game.cenas[game.cena_atual].itens[i].result_negat);
-			end;
-		end
-		else if (game.cenas[game.cena_atual].itens[i].tipo = 2) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
-		begin
-			if (estaNoInv(invent, Split(kbImput, ' ').[1]) = true) and (game.cenas[game.cena_atual].itens[i].resolvido = false) then
-			begin
-				removeDoInv(invent, Split(kbImput, ' ').[1]);
-                                game.cenas[game.cena_atual].itens[i].resolvido := true;
-                                writeln(game.cenas[game.cena_atual].itens[i].result_posit);
-                                game.cena_atual := game.cena_atual + 1;
-                        end
-			else
-			begin
-				writeln(game.cenas[game.cena_atual].itens[i].result_negat);
-			end;
-                end
-                else if (game.cenas[game.cena_atual].itens[i].tipo = 3) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
-		begin
-                	if (estaNoInv(invent, Split(kbImput, ' ').[1]) = true) and (game.cenas[game.cena_atual].itens[i].resolvido = false) then
-			begin
-				removeDoInv(invent, Split(kbImput, ' ').[1]);
-                                game.cenas[game.cena_atual].itens[i].resolvido := true;
-                                writeln(game.cenas[game.cena_atual].itens[i].result_posit);
-				kbImput := 'exit';
-			end;
-		end
-		else if (game.cenas[game.cena_atual].itens[i].tipo = 4) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
-		begin
-			writeln(game.cenas[game.cena_atual].itens[i].result_posit);
-			kbImput := 'exit';
-		end
-		else if (game.cenas[game.cena_atual].itens[i].tipo = 5) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
-		begin
-			writeln(game.cenas[game.cena_atual].itens[i].result_posit);	
-		end
-		else
-		begin
-			writeln(game.cenas[game.cena_atual].itens[i].result_negat);
-		end;
-		
-	end; // while
+		        begin
+               	    if game.cenas[game.cena_atual].itens[i].resolvido = false then
+                    begin
+		      		    game.cenas[game.cena_atual].itens[i].resolvido := true;
+			     	    write(game.cenas[game.cena_atual].itens[i].result_posit);
+				        game.cena_atual := game.cena_atual + 1;
+                        write('.');Delay(500);write('.');Delay(500);writeln('.');Delay(500);
+                    end;
+		    end
+		    else if (game.cenas[game.cena_atual].itens[i].tipo = 2) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
+		    begin
+			    if (estaNoInv(invent, Split(kbImput, ' ').[1]) = true) and (game.cenas[game.cena_atual].itens[i].resolvido = false) then
+			    begin
+				    invent := removeDoInv(invent, Split(kbImput, ' ').[1]);
+                    game.cenas[game.cena_atual].itens[i].resolvido := true;
+                    write(game.cenas[game.cena_atual].itens[i].result_posit);
+                    game.cena_atual := game.cena_atual + 1;
+                    write('.');Delay(500);write('.');Delay(500);writeln('.');Delay(500);
+                end;
+            end
+            else if (game.cenas[game.cena_atual].itens[i].tipo = 3) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
+		    begin
+           	    if (estaNoInv(invent, Split(kbImput, ' ').[1]) = true) and (game.cenas[game.cena_atual].itens[i].resolvido = false) then
+			    begin
+				    removeDoInv(invent, Split(kbImput, ' ').[1]);
+                    game.cenas[game.cena_atual].itens[i].resolvido := true;
+                    //gameOver();
+                    writeln(game.cenas[game.cena_atual].itens[i].result_posit);
+                    Delay(12000);
+                    kbImput := 'exit';
+                end;
+            end
+            else if (game.cenas[game.cena_atual].itens[i].tipo = 4) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
+            begin
+                //gameOver();
+                writeln(game.cenas[game.cena_atual].itens[i].result_posit);
+                Delay(12000);
+			    kbImput := 'exit';
+		    end
+		    else if (game.cenas[game.cena_atual].itens[i].tipo = 5) and (kbImput = game.cenas[game.cena_atual].itens[i].comand_correct) then
+		    begin
+			    writeln(game.cenas[game.cena_atual].itens[i].result_posit);
+		    end;
+		    //else
+		    //begin
+			  //  writeln(game.cenas[game.cena_atual].itens[i].result_negat);
+		    //end;
+
+	    end; // for
+    end; // while
 end; // procedure
 
+procedure mainScreen();
+begin
+    writeln();
+    writeln('Em um mundo onde fantasmas, espiritos e seres sobrenaturais sao reais, cada noite eh uma batalha pela sobrevivencia, pois eh a noite que esses seres malignos saem para aterrorizarem suas vitimas. Para aqueles que nao conseguem se abrigar seguramente durante as longas e terriveis noites, apenas a morte esta reservada.');
+    writeln('Voce realmente deseja comecar?');                                   
+    writeln();
+    writeln('Digite:');
+    writeln('-> start');
+    writeln('-> help');
+    writeln('-> load');
+    writeln('-> exit');    
+end;
+
+
+procedure startGame();
 var
+    arq : text;
+    linha : string;
+begin
+    Assign(arq,'./Files/Titulo.txt');
+    reset(arq);
+    while not EOF(arq) do
+        begin
+          readln(arq, linha);
+          writeln(linha);
+          Delay(150);
+    end;
+    close(arq);
+
+end;
+
+procedure startMusic(musica : string);
+var
+   RunProgram: TProcess;
+    begin
+        RunProgram := TProcess.Create(nil);
+        RunProgram.CommandLine := 'play -q ' + musica ;
+        RunProgram.Execute;
+end;
+
+procedure stopMusic();
+var
+   RunProgram2: TProcess;
+begin
+        RunProgram2 := TProcess.Create(nil);
+        RunProgram2.CommandLine := 'killall play';
+        RunProgram2.Execute;
+        RunProgram2.Free;
+end;
+
+
+
+var
+        arq : text;
         game : Jogo;
-        title : text;
-        linha, kbImput, um : string;
+        loading : integer;
+        inicial, musica :string;
 
 begin
-clrscr();                                       //Limpa o console
-
-        Assign(title,'./Files/Title.txt');
-        reset(title);
-        while not EOF(title) do
-        begin
-          readln(title, linha);
-          writeln(linha);
-          Delay(200);
-        end;
-        close(title);
-
-        clrscr();                               //Limpa o console
-        Delay(1000);
+    clrscr();
+    inicial := 'nada';
+    loading := 0;
+    musica := 'Files/FundoTerror.mp3';
+    
+    startGame();
+    startMusic(musica);
+    while inicial <> 'exit' do
+    begin
+        mainScreen();
+        readln(inicial);
+        
 
         game := criaJogo();
 
-        //printaCena(game.cenas[1]);
-        motorzera(game);
-        readln(kbImput);                        //so para congelar o texto
-
-        //Delay(1000);
-
+        if inicial = 'start' then
+        begin
+            clrscr();                              //Lim  pa o console
+            write('.');Delay(500);write('.');Delay(500);write('.');Delay(500);
+            clrscr();
+            motorzera(game, 0);
+        end    //else if igual a load e etc..
+        else if inicial = 'load' then
+        begin
+            Assign(arq,'./Files/Save.txt');
+            reset(arq);
+            readln(arq, loading);
+            motorzera(game, loading);
+        end
+        else if inicial = 'help' then
+        begin
+            PrintHelp();
+            readln(inicial);
+        end;
+        clrscr();
+    end;
+    stopMusic();
+    
 end.
+
